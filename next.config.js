fb2d95ca5a83b4ab940a51bdc8a9a328a01dd2c9
alexandre gvozdenovic <alexandre.gvozdenovic@gmail.com>
2022-12-01 20:12:49 +0100

    hosting stuff

--- next.config.js
+++ next.config.js
@@ -6,7 +6,9 @@ const nextConfig = {
   },
   images: {
     domains: ["wl-lizee-production.cellar-c2.services.clever-cloud.com"],
+    unoptimized: true,
   },
+  distDir: "build",
 };
 
 module.exports = nextConfig;
